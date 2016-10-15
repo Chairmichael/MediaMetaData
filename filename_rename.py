5ce777251dc9931b04db7598114a11bde0923c26
Jefferson Henry <jeffvhenry@gmail.com>
2016-10-14 20:49:53 -0500

    Things

--- filename_rename.py
+++ filename_rename.py
@@ -5,9 +5,6 @@ from mutagen.mp3 import EasyMP3 as MP3
 directory = askdirectory()
 os.chdir(directory)
 files = [x for x in os.listdir() if x.endswith('.mp3')]
-
-
-'''	
 for file in files:
 	audio = MP3(file)
 	title = ''.join(audio['title'])
@@ -15,6 +12,7 @@ for file in files:
 	composer = ''.join(audio['composer'])
 	os.rename(file,'%s__%s__%s.mp3' % (composer, title, artist))
 	
+'''
 audio = MP3('MozartPianoSong1.mp3')
 title = ''.join(audio['title'])
 artist = ''.join(audio['artist'])
