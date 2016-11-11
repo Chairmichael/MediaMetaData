a69475823d8375c9ddb969b0be19807f07cb2167
Jefferson V. Henry <jeffvhenry@gmail.com>
2016-11-11 15:52:50 -0600

    Did some work on some methods

--- song.py
+++ song.py
@@ -2,43 +2,54 @@ from mutagen.easyid3 import EasyID3
 #print(EasyID3.valid_keys.keys())
 
 class Song(object):
-	def __init__(self):
-		print('  ^^In Song __init__^^')
+	def __init__(self, file_name = None, aid = None):
+		#print('  ^^In Song __init__^^')
 		#self.data = { }
 		# Gets a list the supported EasyID3 keys
 		keys = str(EasyID3.valid_keys.keys())[12:-3].split("', '")
 		# Puts the supported keys into a dictionary with 'None' values
 		self.data = {}.fromkeys(keys)
-		print('  ^^Set self.data^^')
+		#print('  ^^Set self.data^^')
 
-# Gets a substring between <a> and <b> 
-# Mode: The tuple is if it should use rfind to find <a> and <b> respectively
-def get_between(s, first, last, mode = (False, False)):
-	if mode is (False, False): # Use find for both
-		try:
-			start = s.index(first) + len(first)
-			end = s.index(last, i)
-			return s[i:j]
-		except ValueError:
-			return ''
-	if mode is (False, True): # Use rfind for last
-		try:
-			start = s.index(first) + len(first)
-			end = s.rindex(last)
-			return s[i:j]
-		except ValueError:
-			return ''
-	if mode is (True, False): # Use rfind for first
-		try:
-			start = s.rindex(first) + len(first)
-			end = s.index(last, i)
-			return s[i:j]
-		except ValueError:
-			return ''
-	if mode is (True, True): # Use rfind for both
-		try:
-			start = s.rindex(first) + len(first)
-			end = s.rindex(last)
-			return s[i:j]
-		except ValueError:
-			return ''
+	def get_line_with(self, file_aid, s):
+	# try:
+		with open(file_aid) as file:
+			for line in file:
+				# Find the line that has the catalognumber
+				i = line.find(s)
+				if i is not -1: # If right line...
+					return line
+	# except Error:
+	# 	return None
+
+	# Gets a substring between <a> and <b> 
+	# Mode: The tuple is if it should use rfind to find <a> and <b> respectively
+	def get_between(self, s, first, last, mode = (False, False)):
+		if mode is (False, False): # Use find for both
+			try:
+				start = s.index(first) + len(first)
+				end = s.index(last, i)
+				return s[i:j]
+			except ValueError:
+				return ''
+		if mode is (False, True): # Use rfind for last
+			try:
+				start = s.index(first) + len(first)
+				end = s.rindex(last)
+				return s[i:j]
+			except ValueError:
+				return ''
+		if mode is (True, False): # Use rfind for first
+			try:
+				start = s.rindex(first) + len(first)
+				end = s.index(last, i)
+				return s[i:j]
+			except ValueError:
+				return ''
+		if mode is (True, True): # Use rfind for both
+			try:
+				start = s.rindex(first) + len(first)
+				end = s.rindex(last)
+				return s[i:j]
+			except ValueError:
+				return ''
--- symphony.py
+++ symphony.py
@@ -2,10 +2,44 @@ from song import get_between
 from song import Song
 
 class Mozart_Symphony(Song):
-	def __init__(self, file_name):
+	def __init__(self, file_name, aid):
 		super(Mozart_Symphony,self).__init__()
+		self.file_name = file_name
+		self.aid = aid
+		# Use helper methods for readability
+		self.nickname = self.__get_nickname()
+		# Set some data
 		self.data['genre'] = 'Classical'
-		print(self.data['genre'])
+		self.data['composer'] = 'Mozart, Wolfgang Amadeus'
+		self.data['artist'] = 'Mozart, Wolfgang Amadeus'
+		self.data['album'] = 'Mozart Symphonies'
+		# Use some more helper methods for readability
+		self.data['catalognumber'] = self.__get_catalog_number()
+		self.data['tracknumber'] = self.__get_movement_number()
+		self.data['discnumber'] = self.__get_symphony_number()
+
+	# Helper method to get the nickname from the aid file
+	def __get_nickname(self):
+		if self.get_line_with(self.aid, 'K. ' + self.data['catalognumber'])
+			name = get_between(line, '<br />"', '"')
+			if len(name) < 15:
+				self.nickname = '(%s)' % name
+
+	# Helper method to get the kochel catalog number from the aid file
+	def __get_catalog_number(self):
+		return get_between(file_name, ' K ', ' ')
+
+	# Helper method to get the symphony number
+	def __get_symphony_number(self):
+		with open(os.getcwd() + '\\symphonies.txt') as symphonies_data:
+			for line in symphonies_data:
+				i = line.find('K. ' + self.k_number)
+				if i is not -1:
 
 
-Mozart_Symphony('')
+	# Helper method to get the movement number
+	def __get_movement_number(self):
+		return None
+
+
+Mozart_Symphony('', '')
