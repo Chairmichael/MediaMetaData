bf9e794e05e040388897bad41642a12227b1cd05
Jefferson Henry <jeffvhenry@gmail.com>
2016-11-12 16:36:25 -0600

    Removed everything, and made it simple again

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-import os, sys
-from tkinter.filedialog import askdirectory
+from mutagen.easyid3 import EasyID3
 
-from song import Song
-#from databuilder import Databuilder
-
-def main():
-	# Starting directory for reference
-	starting_directory = os.getcwd()
-	# Get directory for the files to change
-	directory = askdirectory()
-	# Take only .mp3 files
-	file_names = [x for x in os.listdir() if x.endswith('.mp3')]
-
-
-if __name__ == '__main__': main()
